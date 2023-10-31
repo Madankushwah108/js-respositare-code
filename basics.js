74c2a059dbc845e1a15eed104437c89cf6ab8cdf
Madankushwah108 <149192305+Madankushwah108@users.noreply.github.com>
2023-10-31 17:50:20 +0000

    basics.js

---
+++ basics.js
@@ -0,0 +1,8 @@
+const AccountId =757834
+let accountCity ="gwalior"
+var accountName ="madan"
+let accountPassword ="648717"
+let accountState;
+
+/*count = not allowed 2 times*/
+console.table([AccountId, accountCity, accountName, accountPassword, accountState]);
